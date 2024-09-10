71a5256703aee1b6c2d7fd80469797f782e3157d
DESKTOP-TI6TLN5\USER <adsa77@naver.com>
2024-09-10 17:35:28 +0900

    기본설정 파일들

---
+++ app/src/main/java/co/kr/fulloftraval/FulloftravalApplication.java
@@ -0,0 +1,13 @@
+package co.kr.fulloftraval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulloftravalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulloftravalApplication.class, args);
+	}
+
+}
